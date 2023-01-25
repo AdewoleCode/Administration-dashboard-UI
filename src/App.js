99a554c6a42210db94e6c4e690b8030240f61274
adewole <adeadewole1712@gmail.com>
2023-01-25 23:31:28 +0100

    created routes, navbar and sideways components

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import New from "./pages/newPage/New";
+import Single from "./pages/singleUser/Single";
+import List from "./pages/usersList/List";
+
+function App() {
+  return (
+    <div>
+
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/users" element={<List/>} />
+        <Route path="users/:userId" element={<Single/>} />
+        <Route path="users/new" element={<New/>} />
+        <Route path="/products" element={<List/>} />
+        <Route path="/products/:productId" element={<Single/>} />
+        <Route path="/products/new" element={<New/>} />
+
+
+
+
+      </Routes>
+      
+      </BrowserRouter>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Home.scss"
+import Sidebar from '../../components/sidebar/Sidebar'
+
+const Home = () => {
+  return (
+    <div>
+        <Sidebar />
+    </div>
+  )
+}
+
+export default Home
