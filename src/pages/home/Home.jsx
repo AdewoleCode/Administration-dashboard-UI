cff4a93aada6c29d118b4584746f34289f01f93b
adewole <adeadewole1712@gmail.com>
2023-01-26 00:14:22 +0100

    created reusable widget component and rendered in homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import "./Home.scss"
 import Sidebar from '../../components/sidebar/Sidebar'
+import NavBar from "../../components/navbar/Navbar"
+import Widget from '../../components/widget/Widget'
 
 const Home = () => {
   return (
-    <div>
+    <div className="home">
         <Sidebar />
+        <div className="homeContainer">
+          <NavBar />
+          <div className="widgets">
+            <Widget type="user" />
+            <Widget  type="order"/>
+            <Widget  type="earning"/>
+            <Widget  type="balance"/>
+          </div>
+        </div>
     </div>
   )
 }
