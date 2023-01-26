e584bb0074d255e18f82ea87756af20323594063
adewole <adeadewole1712@gmail.com>
2023-01-26 23:20:56 +0100

    created a datatable component with mui table

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import NavBar from '../../components/navbar/Navbar'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Datatable from '../../components/datatable/DataTable'
+import "../../pages/list/List.scss"
 
 const List = () => {
   return (
-    <div>List</div>
+    <div className='list'>
+      <Sidebar />
+      <div className="listContainer">
+        <NavBar />
+        <Datatable />
+      </div>
+    </div>
   )
 }
 
